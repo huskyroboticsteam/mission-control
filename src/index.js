cb96747e6d386b38905bec0ab810c4a7618d2506
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2021-12-27 22:02:01 -0800

    Transfer contents of index.css to App.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./components/App";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
