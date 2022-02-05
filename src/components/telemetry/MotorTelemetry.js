e944f4a62940e3a39fc1c9ff93fc832c4e99c152
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2022-02-05 12:29:53 -0800

    Style motor telemetry

--- src/components/telemetry/MotorTelemetry.js
+++ src/components/telemetry/MotorTelemetry.js
@@ -1,5 +1,9 @@
 import { useSelector } from "react-redux";
-import { selectAllMotorNames, selectMotorCurrentPosition, selectMotorCurrentPower, selectMotorCurrentVelocity } from "../../store/motorsSlice";
+import {
+  selectAllMotorNames,
+  selectMotorCurrentPosition,
+  selectMotorCurrentPower
+} from "../../store/motorsSlice";
 import camelCaseToTitle from "../../util/camelCaseToTitle";
 import "./MotorTelemetry.css";
 
@@ -11,11 +15,11 @@ function MotorTelemetry() {
       <table>
         <thead>
           <tr>
-              <th>Motor</th>
-              <th>Power</th>
-              <th>Position (°)</th>
-            </tr>
-          </thead>
+            <th>Motor</th>
+            <th>Power</th>
+            <th>Position</th>
+          </tr>
+        </thead>
         <tbody>
           {motorNames.map(motorName => <MotorData motorName={motorName} key={motorName} />)}
         </tbody>
@@ -29,19 +33,11 @@ function MotorData({ motorName }) {
   const position = useSelector(selectMotorCurrentPosition(motorName));
   const motorTitle = camelCaseToTitle(motorName);
 
-  const textColor = {};
-  if (power) {
-    const red = (1 - power) * 255;
-    const green = power * 255;
-    const blue = 0;
-    textColor.color = `rgb(${red}, ${green}, ${blue})`;
-  }
-
   return (
     <tr className="motor-telemetry__motor-data">
       <td>{motorTitle}</td>
-      <td style={textColor}>{power != null ? power : "N/A"}</td>
-      <td>{position != null ? Math.round(position) : "N/A"}</td>
+      <td>{power != null ? power : "N/A"}</td>
+      <td>{position != null ? `${Math.round(position)}°` : "N/A"}</td>
     </tr>
   );
 }
