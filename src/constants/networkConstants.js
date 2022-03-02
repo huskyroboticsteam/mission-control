115942995b1e5f18dc582f5d1e43196f10f005a2
Connor Reinholdtsen <43483846+connorrein@users.noreply.github.com>
2022-03-01 18:54:26 -0800

    Add real rover URL to networkConstants.js

--- src/constants/networkConstants.js
+++ src/constants/networkConstants.js
@@ -1,4 +1,4 @@
 export const ROVER_SERVER_URL =
   (process.env.NODE_ENV && process.env.NODE_ENV === "development")
     ? "ws://localhost:3001/mission-control"
-    : "ws://insert-production-url-here/mission-control";
+    : "ws://jetson-1.local:3001/mission-control";
