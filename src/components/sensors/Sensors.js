2f9230ca4e485ff8484bc2a5a453e4b6c9f4a133
kpovey <katpovey@uw.edu>
2022-05-09 18:26:53 -0700

    sensor grid

--- src/components/sensors/Sensors.js
+++ src/components/sensors/Sensors.js
@@ -6,8 +6,12 @@ function Sensors () {
     const canvasRef = useRef();
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
+    const soil_humidity = 0;
+    const thermocouple = 0;
+    const uv = 0;
+    const air_quality = 0;
+    const weather = 0;
 
-    
     useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
@@ -34,7 +38,19 @@ function Sensors () {
 
     return (
         <div className="sensors">
-          <h2 className="sensors__sensors-name">Sensors</h2>
+          <h2 className="sensors-name">Sensors</h2>
+          <div className="container">
+            <div className="reading">Sensor 1</div> 
+            <div className="reading">reading</div> 
+            <div className="reading">Sensor 2</div> 
+            <div className="reading">reading</div> 
+            <div className="reading">Sensor 3</div> 
+            <div className="reading">reading</div> 
+            <div className="reading">Sensor 4</div> 
+            <div className="reading">reading</div> 
+            <div className="reading">Sensor 5</div> 
+            <div className="reading">reading</div> 
+          </div>
           <canvas ref={canvasRef} className="syringe" width={width} height={height} />
         </div>
     
