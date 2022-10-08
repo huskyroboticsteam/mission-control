a0695b3989f7cb14ac8d40332a3ce00ce9a7ee2a
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2022-10-08 12:53:03 -0700

    Use new react API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./components/app/App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
