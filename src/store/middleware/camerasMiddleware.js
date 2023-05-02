ec70529e9766ee3e179a14e9c3bb8b412bef11fc
Geeoon Chung <geeoon13@gmail.com>
2023-05-01 18:38:04 -0700

    Added fps to open stream request

--- src/store/middleware/camerasMiddleware.js
+++ src/store/middleware/camerasMiddleware.js
@@ -23,7 +23,7 @@ const camerasMiddleware = store => next => action => {
         message: {
           type: "cameraStreamOpenRequest",
           camera: action.payload.cameraName,
-          fps: 20  // default to 20
+          fps: 20,  // default to 20
         }
       }));
       break;
@@ -49,7 +49,8 @@ const camerasMiddleware = store => next => action => {
           store.dispatch(messageRover({
             message: {
               type: "cameraStreamOpenRequest",
-              camera: cameraName
+              camera: cameraName,
+              fps: 20, // default to 20
             }
           }));
         }
