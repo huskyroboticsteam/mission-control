eaf61a678fd54687e3dbbcf55c88ef2443ce1455
Abhay D <a.deshpande012@gmail.com>
2023-05-29 14:02:35 -0700

    Fix compass math

--- src/components/delivery/Compass.js
+++ src/components/delivery/Compass.js
@@ -2,57 +2,56 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectRoverPosition } from "../../store/telemetrySlice";
 import "./Compass.css";
+import {Quaternion, Euler, Vector3} from '@math.gl/core';
+import * as Quat from 'gl-matrix/quat';
+
+function getAttitude(roll, pitch) {
+  let attitudeRPY = new Euler().fromRollPitchYaw(roll, pitch, 0.0);
+  // taken from Euler.getQuaternion(), which we can't call because it's broken
+  let attitudeQuat = new Quaternion().rotateZ(attitudeRPY.x).rotateX(attitudeRPY.y).rotateY(attitudeRPY.z);
+  let attitude = Quat.getAxisAngle(new Vector3(), attitudeQuat);
+  return attitude;
+}
 
 const Compass = () => {
   const {orientW, orientX, orientY, orientZ, posX, posY, posZ} = useSelector(selectRoverPosition);
 
   let roll;
   let pitch;
+  let yaw;
   let needleColor;
 
   if (orientW == null || orientX == null || orientY == null || orientZ == null) {
     needleColor = "gray";
   } else {
-    roll = Math.atan2(2.0 * (orientX * orientY + orientW * orientZ), orientW * orientW + orientX * orientX - orientY * orientY - orientZ * orientZ);
-    pitch = Math.asin(-2.0 * (orientZ * orientZ - orientW * orientY));
-    const c2 = Math.cos(pitch / 2);
-    const c3 = Math.cos(roll / 2);
+    let quat = new Quaternion(orientX, orientY, orientZ, orientW);
+    let rpy = new Euler().fromQuaternion(quat, Euler.ZYX);
+    let attitude = getAttitude(rpy.roll, rpy.pitch);
+  
+    roll = Math.round(rpy.roll * 180 / Math.PI);
+    pitch = Math.round(rpy.pitch * 180 / Math.PI);
+    yaw = Math.round(rpy.yaw * 180 / Math.PI);
 
-    // angle = 2 * acos(c1c2c3 + s1s2s3)
-    // c1 = 1 in all cases, since yaw = 0
-    // s1s2s3 = 0 in all cases, since yaw = 0
-    // math comes from here: https://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToAngle/index.htm
-    const angle = (2 * Math.acos(c2 * c3) * 180) / Math.PI;
-
-    roll = Math.round(roll * 180 / Math.PI);
-    pitch = Math.round(pitch * 180 / Math.PI);
-
-    if (Math.abs(angle) < 20) {
+    if (Math.abs(attitude) < 20) {
       needleColor = "green";
-    } else if (Math.abs(angle) < 45) {
+    } else if (Math.abs(attitude) < 45) {
       needleColor = "yellow";
     } else {
       needleColor = "red";
     }
   }
 
-  const direction = Math.round(
-    (Math.atan2(
-      2 * (orientW * orientZ + orientX * orientY),
-      1 - 2 * (orientY * orientY + orientZ * orientZ)
-    ) * 180) / Math.PI
-  );
-
   const latitude = posX;
   const longitude = posY;
   const altitude = posZ;
+  const heading = yaw ? -yaw : undefined; // yaw is CCW, heading is CW
 
   return (
     <div className="compass-container">
       <div className="compass">
         <div
           className={`compass__needle compass__needle--${needleColor}`}
-          style={{ transform: `rotate(${direction}deg)` }}
+          style={{ transform: `rotate(${heading ?? 0}deg)` }}
         ></div>
         <div className={`compass__outer-ring ${needleColor}`}></div>
         <div className="compass__label compass__label--north">N</div>
@@ -63,6 +62,7 @@ const Compass = () => {
       <div className="info">
         <div>roll: {roll ?? "N/A"}</div>
         <div>pitch: {pitch ?? "N/A"}</div>
+        <div>heading: {heading ?? "N/A"}</div>
         <div>latitude: {latitude ?? "N/A"}</div>
         <div>longitude: {longitude ?? "N/A"}</div>
         <div>altitude: {altitude ?? "N/A"}</div>
