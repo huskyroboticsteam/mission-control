5314328dcebc08841c1effb8d982426190099166
Geeoon Chung <geeoon13@gmail.com>
2023-06-01 19:17:39 -0700

    Disabled ik toggle when rover is not connected

--- src/components/sidebar/ToggleInverseKinematics.js
+++ src/components/sidebar/ToggleInverseKinematics.js
@@ -1,13 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectInverseKinematicsEnabled, enableIK } from "../../store/inputSlice";
+import { selectRoverIsConnected } from "../../store/roverSocketSlice";
 import "./ToggleInverseKinematics.css";
 
 function ToggleInverseKinematics() {
     const dispatch = useDispatch();
     const IKEnabled = useSelector(selectInverseKinematicsEnabled);
+    const roverIsConnected = useSelector(selectRoverIsConnected);
 
     const handleClick = () => {
-        dispatch(enableIK({ enable: !IKEnabled && window.confirm("Turning on inverse kinematics requires that the motors are calibrated.  You must be sure the motors are calibrated or the robot will break.") })); // change dispatch
+        if (roverIsConnected) {
+            dispatch(enableIK({ enable: !IKEnabled && window.confirm("Turning on inverse kinematics requires that the motors are calibrated.  You must be sure the motors are calibrated or the robot will break.") })); // change dispatch
+        }
     };
 
     return (
@@ -17,7 +21,7 @@ function ToggleInverseKinematics() {
             </div>
             <div id='switch-wrapper' onClick={handleClick}>
                 <div id={ IKEnabled ? 'disable-ik' : 'enable-ik'} className='ik-switch'>
-                    <div id={ IKEnabled ? 'disable-ik-handle' : 'enable-ik-handle'} className='switch-handle'>
+                    <div id={IKEnabled ? 'disable-ik-handle' : 'enable-ik-handle'} className={roverIsConnected ? 'switch-handle' : ' switch-handle-disconnected'}>
                     </div>
                 </div>
             </div>
