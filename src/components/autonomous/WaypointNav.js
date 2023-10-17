0aba955d8ab98bc498eac4c9e4a473f2d0d3eebd
Varun <vvijayababu4334@gmail.com>
2023-10-16 18:58:17 -0700

    re enable button when switching to teleop

--- src/components/autonomous/WaypointNav.js
+++ src/components/autonomous/WaypointNav.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
 import { requestWaypointNav } from "../../store/waypointNavSlice";
 import { selectOpMode } from "../../store/opModeSlice";
 import "./WaypointNav.css";
 
 function WaypointNav() {
   const dispatch = useDispatch();
+  const [submitted, setSubmitted] = useState(false);
   var opMode = useSelector(selectOpMode);
 
   function handleSubmit (e) {
@@ -12,32 +14,33 @@ function WaypointNav() {
     const form = e.target;
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
-
-    // Only send the event in autonomous mode
-    if (opMode === "autonomous") {
-        console.log(formJson);
-        dispatch(requestWaypointNav(formJson));
-    }
+    setSubmitted(true);
+    dispatch(requestWaypointNav(formJson));
   };
+
+  useEffect(() => {
+    if (opMode === "teleoperation") {
+      setSubmitted(false);
+    }
+  }, [opMode]);
   
-// move longitude down, move checkboxes togethers
   return (
   <form method="post" onSubmit={handleSubmit} className="waypoint-select"> 
     <div className="waypoint-select__params">
     <label for="latitude">Latitude</label>
-    {opMode === "autonomous" ? <input disabled type="number" name="latitude" placeholder="Latitude" /> : <input type="number" name="latitude" placeholder="Latitude" />}
+    {submitted ? <input disabled type="number" name="latitude" placeholder="Latitude" /> : <input type="number" name="latitude" placeholder="Latitude" />}
       <label for="longitude">Longitude</label>
-      {opMode === "autonomous" ? <input disabled type="number" name="longitude" placeholder="Longitude" /> : <input type="number" name="longitude" placeholder="Longitude" />}
+      {submitted ? <input disabled type="number" name="longitude" placeholder="Longitude" /> : <input type="number" name="longitude" placeholder="Longitude" />}
       <div className="waypoint-checkbox">
         <div>
-          <label>{opMode === "autonomous" ? <input disabled type="checkbox" name="isApproximate" /> : <input type="checkbox" name="isApproximate" />} Approximate</label>
+          <label>{submitted ? <input disabled type="checkbox" name="isApproximate" /> : <input type="checkbox" name="isApproximate" />} Approximate</label>
         </div>
         <div>
-          <label>{opMode === "autonomous" ? <input disabled type="checkbox" name="isGate" />: <input type="checkbox" name="isGate" />} Is Gate</label>
+          <label>{submitted ? <input disabled type="checkbox" name="isGate" />: <input type="checkbox" name="isGate" />} Is Gate</label>
         </div>
       </div>
     </div>
-    {opMode === "autonomous" ? <button type="submit">Go</button> : <button disabled>Go</button>}
+    {opMode === "autonomous" && !submitted ? <button type="submit">Go</button> : <button disabled>Go</button>}
   </form>
   );
 }
