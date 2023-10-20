d3b9de5b1dec68ae3a96af1e47d9b1e183c84c58
Geeoon Chung <geeoon13@gmail.com>
2023-10-19 19:10:00 -0700

    No longer can enable IK if emergency stopped

--- src/components/sidebar/ToggleInverseKinematics.js
+++ src/components/sidebar/ToggleInverseKinematics.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectInverseKinematicsEnabled, enableIK } from "../../store/inputSlice";
 import { selectMotorsAreEnabled } from "../../store/motorsSlice";
 import { selectRoverIsConnected } from "../../store/roverSocketSlice";
+import { selectIsStopped } from '../../store/emergencyStopSlice';
 import "./ToggleInverseKinematics.css";
 
 function ToggleInverseKinematics() {
@@ -9,21 +10,22 @@ function ToggleInverseKinematics() {
     const IKEnabled = useSelector(selectInverseKinematicsEnabled);
     const motorsEnabled = useSelector(selectMotorsAreEnabled);
     const roverIsConnected = useSelector(selectRoverIsConnected);
+    const roverIsStopped = useSelector(selectIsStopped);
 
     const handleClick = () => {
-        if (roverIsConnected && motorsEnabled) {
+        if (roverIsConnected && motorsEnabled && !roverIsStopped) {
             dispatch(enableIK({ enable: !IKEnabled && window.confirm("Turning on inverse kinematics requires that the motors are calibrated.  You must be sure the motors are calibrated or the robot will break.") })); // change dispatch
         }
     };
 
     return (
         <div id='enable-ik-toggle'>
-            <div id={motorsEnabled ? 'enable-ik-title' : 'enable-ik-title-disabled'}>
+            <div id={motorsEnabled && !roverIsStopped ? 'enable-ik-title' : 'enable-ik-title-disabled'}>
                 Enable IK
             </div>
             <div id='switch-wrapper' onClick={handleClick}>
                 <div id={IKEnabled ? 'disable-ik' : 'enable-ik'} className='ik-switch'>
-                    <div id={IKEnabled ? 'disable-ik-handle' : 'enable-ik-handle'} className={roverIsConnected ? 'switch-handle' : ' switch-handle-disconnected'}>
+                    <div id={IKEnabled ? 'disable-ik-handle' : 'enable-ik-handle'} className={roverIsConnected && !roverIsStopped ? 'switch-handle' : ' switch-handle-disconnected'}>
                     </div>
                 </div>
             </div>
