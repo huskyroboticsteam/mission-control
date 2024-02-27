9d556d24388401c72d625cd65275de960960fcd1
Violet Monserate <violetmonserate@gmail.com>
2024-02-27 01:41:28 -0800

    For sar

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Viewer, Globe, Scene, Primitive, Cesium3DTileset, Entity, PointGraphics } from "resium";
-import { IonResource, CesiumTerrainProvider, Cartesian3, Ion } from "cesium"
+import { Viewer, Globe, Scene, Primitive, Cesium3DTileset, Entity, PointGraphics, ImageryLayer, Polyline, PolylineGraphics } from "resium";
+import { IonResource, CesiumTerrainProvider, Cartesian3, Ion, ArcGisMapServerImageryProvider, OpenStreetMapImageryProvider } from "cesium"
 import { useSelector } from "react-redux";
 import { selectRoverLatitude, selectRoverLongitude } from "../../store/telemetrySlice";
 Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NjAyNDE4MS03YzQ5LTQ3YWEtYTA3NS0xZmNlMmMzNjA4MDAiLCJpZCI6MTgwNDExLCJpYXQiOjE3MDA4MDYzODF9.wQNIlvboVB7Zo5qVFUXj2jUMfJRrK_zdvBEp2INt1Kg";
@@ -19,8 +19,10 @@ class TestEntity extends React.Component {
     return <Entity
       name="Rover"
       position={Cartesian3.fromDegrees(-119 + this.state.counter, 48, 0)}
-      point={{ pixelSize: 100 }}
+      point={{ pixelSize: 20 }}
       description={"Lat: " + (-119 + this.state.counter).toFixed(5) + ", Lon: 48"}
+      selected
+      tracked
     />;
   }
 }
@@ -28,7 +30,18 @@ class TestEntity extends React.Component {
 function Map() {  
   const lat = useSelector(selectRoverLatitude);
   const long = useSelector(selectRoverLongitude);
-  
+  const imageryProvider = new OpenStreetMapImageryProvider({
+    url: "https://tile.openstreetmap.org/",
+  });
+  const positions = Cartesian3.fromDegreesArray([
+    -119, 48,
+    -118.9998, 48,
+    -118.999022, 47.999330,
+    -118.998927, 47.999277,
+    -118.998257, 47.998970,
+
+  ]);
+
   return (
     <Viewer  
       style={{overflow:"hidden"}}
@@ -38,13 +51,19 @@ function Map() {
       fullscreenButton={false}
       homeButton={false}
     >
-      {/* <ImageryLayer imageryProvider={
-        new Cesium3DTileset.UrlTemplateImageryProvider({
-          url: 'http://localhost:8080/data/tiles/{z}/{x}/{y}.jpg'
-        })  
-      } /> */}
-      <TestEntity />
-      
+      <TestEntity>
+      </TestEntity>
+      <Entity
+        name="Path 1"
+        description={"Example description :)"}  
+      >
+        <PolylineGraphics
+            show={true}
+            width={10}
+            positions={positions}
+            clampToGround={true}
+          />
+      </Entity>
     </Viewer>
   );
 }
