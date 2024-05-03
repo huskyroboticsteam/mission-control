3020e1c5e328dfc4d542751f1b65c71da529ad13
Geeoon Chung <geeoon13@gmail.com>
2024-05-02 18:55:57 -0700

    Added set waypoint button, different from Go button

--- src/store/waypointNavSlice.js
+++ src/store/waypointNavSlice.js
@@ -12,16 +12,19 @@ const waypointNavSlice = createSlice({
   initialState,
   reducers: {
     requestWaypointNav(state, action) {
-      const {latitude, longitude, isApproximate, isGate} = action.payload
-      state.latitude = typeof latitude == 'string' ? Number.parseFloat(latitude) : latitude
-      state.longitude = typeof longitude == 'string' ? Number.parseFloat(longitude) : longitude
-      state.isApproximate = !!isApproximate
-      state.isGate = !!isGate
+      const { latitude, longitude, isApproximate, isGate } = action.payload;
+      state.isApproximate = !!isApproximate;
+      state.isGate = !!isGate;
     },
-  },
-})
+    setWaypointPosition(state, action) {
+      const { latitude, longitude } = action.payload;
+      state.latitude = typeof latitude == "string" ? Number.parseFloat(latitude) : latitude;
+      state.longitude = typeof longitude == "string" ? Number.parseFloat(longitude) : longitude;
+    }
+  }
+});
 
-export const {requestWaypointNav} = waypointNavSlice.actions
+export const { requestWaypointNav, setWaypointPosition } = waypointNavSlice.actions;
 
 export const selectLatitude = state => state.waypointNav.latitude;
 export const selectLongitude = state => state.waypointNav.longitude;
