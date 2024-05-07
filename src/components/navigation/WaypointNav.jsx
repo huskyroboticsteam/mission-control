d38d239b6d5faabc86fb58db64438074358b47e7
Geeoon Chung <geeoon13@gmail.com>
2024-05-06 18:46:01 -0700

    Updated disabled logic and display current waypoint

--- src/components/navigation/WaypointNav.jsx
+++ src/components/navigation/WaypointNav.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useState, useEffect, useCallback } from "react";
-import { requestWaypointNav, setWaypointPosition } from "../../store/waypointNavSlice";
+import { requestWaypointNav, setWaypointPosition, selectLatitude, selectLongitude } from "../../store/waypointNavSlice";
 import { selectOpMode } from "../../store/opModeSlice";
 import "./WaypointNav.css";
 
@@ -9,7 +9,12 @@ function WaypointNav() {
   const [submitted, setSubmitted] = useState(false);
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
-  var opMode = useSelector(selectOpMode);
+
+  const [isWaypointSet, setIsWaypointSet] = useState(false);
+
+  const storedLat = useSelector(selectLatitude);
+  const storedLon = useSelector(selectLongitude);
+  const opMode = useSelector(selectOpMode);
 
   const handleWaypoint = useCallback(() => {
     dispatch(setWaypointPosition({
@@ -49,22 +54,33 @@ function WaypointNav() {
       setSubmitted(false);
     }
   }, [opMode]);
+
+  useEffect(() => {
+    setIsWaypointSet(storedLat !== null && storedLon !== null);
+  }, [storedLat, storedLon]);
   
   return (
-  <form method="post" onSubmit={handleSubmit} className="waypoint-select"> 
+  <form method="post" onSubmit={handleSubmit} className="waypoint-select">
+    <div>
+      Current Waypoint: {isWaypointSet ? <span style={{fontWeight: "bold"}}>{ storedLat }, { storedLon }</span> : <span style={{fontWeight: "bold", color: "red"}}>Unset</span>}
+    </div>
     <div className="waypoint-select__params">
       <label htmlFor="latitude">Latitude</label>
-      {submitted ? <input disabled value={lat} onChange={e => e}/> : <input type="number" step="any" name="latitude" value={lat} onChange={e => setLat(e.target.value)}/>}
+      <input disabled={isWaypointSet} type="number" step="any" name="latitude" value={lat} onChange={e => setLat(e.target.value)}/>
       <label htmlFor="longitude">Longitude</label>
-      {submitted ? <input disabled value={lon} onChange={e => e}/> : <input type="number" step="any" name="longitude" value={lon} onChange={e => setLon(e.target.value)}/>}
-      {submitted ? <button disabled>Copy from Clipboard</button> : <button type="button" onClick={grabFromClipboard}>Copy from Clipboard</button>}
+      <input disabled={isWaypointSet} type="number" step="any" name="longitude" value={lon} onChange={e => setLon(e.target.value)}/>
+      <button disabled={isWaypointSet} type="button" onClick={grabFromClipboard}>Copy from Clipboard</button>
     </div>
     <div className="waypoint-checkbox">
-      <label>{submitted ? <input disabled type="checkbox" name="isApproximate" /> : <input type="checkbox" name="isApproximate" />} Approximate</label>
-      <label>{submitted ? <input disabled type="checkbox" name="isGate" />: <input type="checkbox" name="isGate" />} Is Gate</label>
+      <label><input disabled={isWaypointSet} type="checkbox" name="isApproximate" /> Approximate</label>
+      <label><input disabled={isWaypointSet} type="checkbox" name="isGate" /> Is Gate</label>
     </div>
-    {!submitted ? <button type="button" onClick={handleWaypoint}>Set Waypoint</button> : <button disabled type="button">Set Waypoint</button>}
-    {opMode === "autonomous" && !submitted ? <button type="submit">Go</button> : <button disabled>Go</button>}
+    {
+      isWaypointSet ?
+        <button disabled={submitted} type="button" onClick={() => { dispatch(setWaypointPosition({ longitude: null, latitude: null })); }}>Unset Waypoint</button> :
+        <button type="button" onClick={handleWaypoint}>Set Waypoint</button>
+    }
+    <button disabled={opMode !== "autonomous" || submitted || !isWaypointSet} type="submit">Go</button>
   </form>
   );
 }
