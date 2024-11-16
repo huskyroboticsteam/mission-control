edbf4d41d6c139660bc025062556bfd23e89d2db
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2024-11-16 13:19:37 -0800

    add a div which shows itself when opMode is autonomous, and displays lon and lat as text

--- src/components/navigation/OpModeSelect.jsx
+++ src/components/navigation/OpModeSelect.jsx
@@ -1,29 +1,59 @@
 import { useDispatch, useSelector } from "react-redux";
 import { requestOpMode, selectOpMode } from "../../store/opModeSlice";
 import "./OpModeSelect.css";
+import { selectRoverPosition } from "../../store/telemetrySlice";
+
+
+function sanitize(num, decimals) {
+  if (num == null) {
+    return "N/A";
+  }
+
+  let ret = num.toString();
+  if (decimals !== undefined) {
+    ret = num.toFixed(decimals);
+  }
+
+  return num >= 0 ? " " + ret : ret;
+}
 
 function OpModeSelect() {
-  const dispatch = useDispatch();
-  const opMode = useSelector(selectOpMode);
+    const dispatch = useDispatch();
+    const opMode = useSelector(selectOpMode);
+    const {lon, lat } = useSelector(selectRoverPosition);
+  
+    const handleClick = () => {
+      if (opMode === "teleoperation") {
+        dispatch(requestOpMode({ mode: "autonomous" }));
+      } else if (opMode === "autonomous") {
+        dispatch(requestOpMode({ mode: "teleoperation" }));
+      }
+    };
 
-  const handleClick = () => {
-    if (opMode === "teleoperation") {
-      dispatch(requestOpMode({ mode: "autonomous" }));
-    } else if (opMode === "autonomous") {
-      dispatch(requestOpMode({ mode: "teleoperation" }));
-    }
-  };
+    
 
-  return (
-    <div className={`op-mode-select op-mode-select--${opMode}`}>
-      <p>
-        Current operation mode: <span className={`op-mode-select__op-mode op-mode-select__op-mode--${opMode}`}>{opMode}</span>
-      </p>
-      <button onClick={handleClick} >
-        Switch to {opMode === "teleoperation" ? "Autonomous" : "Teleoperation"}
-      </button>
-    </div >
-  );
-}
+
+    
+  
+    return (
+      <div className={`op-mode-select op-mode-select--${opMode}`}>
+        <p>
+          Current operation mode: <span className={`op-mode-select__op-mode op-mode-select__op-mode--${opMode}`}>{opMode}</span>
+        </p>
+        <button onClick={handleClick} >
+          Switch to {opMode === "teleoperation" ? "Autonomous" : "Teleoperation"}
+        </button>
+        {
+          opMode === "autonomous" ?
+            <div className="nav-status">
+              <p>Longitude: {sanitize(lon)}</p>
+              <p>Latitude: {sanitize(lat)}</p>
+            </div>
+          : <></>
+        }
+        
+      </div >
+    );
+  }
 
 export default OpModeSelect;
