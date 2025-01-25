7ed6652043bc48735905e42a6d3f86ec6704f671
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2025-01-25 12:52:01 -0800

    add keybindings

--- src/store/inputSlice.js
+++ src/store/inputSlice.js
@@ -52,7 +52,8 @@ const initialState = {
       ikForward: 0,
     },
     science: {
-      lazySusanPosition: 0
+      lazySusanPosition: 0,
+      instrumentationArm: 0
     }
   },
   inverseKinematics: {
@@ -251,6 +252,7 @@ function computeScienceInput(prevState, state, action) {
   if (lazySusanAxis !== prevLazySusanAxis)
     scienceInput.lazySusanPosition = (((scienceInput.lazySusanPosition +
       lazySusanAxis) % 6) + 6) % 6;
+  scienceInput.instrumentationArm = getAxisFromKeys(pressedKeys,"C", "V");
 }
 
 function getAxisFromButtons(gamepad, negativeButton, positiveButton) {
