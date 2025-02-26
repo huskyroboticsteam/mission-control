aee0b5393a2ea54979ada4f968161d386bab2e53
Jason Zhang <zjjc123@hotmail.com>
2025-02-26 14:42:07 -0800

    clean up

--- src/components/help/HelpPanel.jsx
+++ src/components/help/HelpPanel.jsx
@@ -1,13 +1,10 @@
 import "./HelpPanel.css";
-import { useSelector } from "react-redux";
 import standardDriveControls from "./standardDriveControls.png";
 import tankDriveControls from "./tankDriveControls.png";
 import armControls from "./armControls.png";
 import KeyboardMapping from "../input/KeyboardMapping";
 
 function HelpPanel() {
-  const pressedKeys = useSelector((state) => state.input.keyboard.pressedKeys);
-
   return (
     <div className="help-panel">
       <div className="controls-images">
