14aad1067e0b872cbd3170001136e4f7a28b8d75
Eliana <yunmin626@gmail.com>
2025-04-17 17:29:00 -0700

    Functional buttons

--- src/components/science/SciencePanel.jsx
+++ src/components/science/SciencePanel.jsx
@@ -1,12 +1,35 @@
 import CameraStream from '../camera/CameraStream'
-
 import './SciencePanel.css'
+import { useDispatch, useSelector } from 'react-redux' // Import Redux hooks
+import { requestJointPower, selectJointCurrentPosition } from '../../store/jointsSlice' // Import actions and selectors from jointsSlice
 
 function ArmDexterityPanel() {
+  const dispatch = useDispatch()
+  const isDrillOn = useSelector(state => selectJointCurrentPosition('drillMotor')(state)) === 'on' || false // Get drill state from Redux store
+  console.log('isDrillOn:', isDrillOn)
+
+  const drillOnClick = () => {
+    dispatch(requestJointPower({ jointName: 'drillMotor', power: 'on' })) // Dispatch action to turn drill on
+    console.log('Drill is now ON')
+  }
+
+  const drillOffClick = () => {
+    dispatch(requestJointPower({ jointName: 'drillMotor', power: 'off' })) // Dispatch action to turn drill off
+    console.log('Drill is now OFF')
+  }
+
   return (
     <div className="science-panel">
       <CameraStream cameraName="pano" />
       <CameraStream cameraName="drill" />
+      <div className="drill-controls">
+        <button onClick={drillOnClick} disabled={isDrillOn}>
+          Drill On
+        </button>
+        <button onClick={drillOffClick} disabled={!isDrillOn}>
+          Drill Off
+        </button>
+      </div>
     </div>
   )
 }
--- src/store/jointsSlice.js
+++ src/store/jointsSlice.js
@@ -1,18 +1,20 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 const jointNames = [
-  'armBase',
-  'shoulder',
-  'elbow',
-  'forearm',
-  'wristPitch',
-  'wristRoll',
-  'hand',
-  'handActuator',
-  'drillArm',
-  'ikUp',
-  'ikForward',
-]
+  "armBase",
+  "shoulder",
+  "elbow",
+  "forearm",
+  "wristPitch",
+  "wristRoll",
+  "hand",
+  "handActuator",
+  "drillArm",
+  "drillMotor",
+  "activeSuspension",
+  "ikUp",
+  "ikForward"
+];
 
 const initialState = jointNames.reduce(
   (state, jointName) => ({
@@ -31,9 +33,14 @@ const jointsSlice = createSlice({
   initialState,
   reducers: {
     requestJointPower(state, action) {
-      const {jointName, power} = action.payload
-      const joint = state[jointName]
-      joint.requestedPower = power
+      const { jointName, power } = action.payload;
+      const joint = state[jointName];
+      joint.requestedPower = power;
+      if (power === 'on'){
+        joint.currentPosition = 'on';
+      } else if (power === 'off'){
+        joint.currentPosition = 'off';
+      }
     },
 
     requestJointPosition(state, action) {
