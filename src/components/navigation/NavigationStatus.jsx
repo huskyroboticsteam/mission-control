de77bd8a91bf3289871027b9e243e71764c5b5d4
MagmaOverlord <tomas.brejl@gmail.com>
2025-05-20 18:13:52 -0700

    added edit, move up, and move down functionality

--- src/components/navigation/NavigationStatus.jsx
+++ src/components/navigation/NavigationStatus.jsx
@@ -23,7 +23,6 @@ function NavigationStatus() {
   const targetPoints = useSelector(selectPoints)
   const targetLatitude = Array.isArray(targetPoints[0]) ? parseFloat(targetPoints[0][0]) : null
   const targetLongitude = Array.isArray(targetPoints[0]) ? parseFloat(targetPoints[0][1]) : null
-  
 
   const getNavigationStatus = () => {
     // Guard against null or undefined values
@@ -41,17 +40,20 @@ function NavigationStatus() {
     if (distance <= POSITION_THRESHOLD) {
       //NOTE: There are plans to update the data the rover sends to include the target point and the naviagion status. As such, most of this
       //wouldn't be necessary. It's here for now, but remove it once that comes to fruition.
-      
+
       //IMPORTANT NOTE: THE targetPoints VARIABLE IS SOMEWHAT DISCONNECTED FROM THE STATE. REMOVING VALUES HERE WILL NOT REMOVE THEM FROM THE STATE
       //THIS SHOULD BE FINE BECAUSE THE STATE IS NOT MODIFIED, ONLY REPLACED, WHICH SHOULD UPDATE targetPoints, AND THE ONLY TIME THE STATE IS
       //UPDATED IS WHEN IT GETS AN ENTIRELY NEW ARRAY
       //checking that the array doesn't include the location the rover is at, and if does, delete it
       //not sure how often this check is run, but I imagine running this if constantly won't be the greaters for performance
       //there is potential to have buggy point removal, better method would be for rover to send the point to be removed & to see if the first point is the same
-      console.log("Latitude distance: " + Math.abs(lat - targetPoints[0][0]))
-      console.log("Longitude distance: " + Math.abs(lon - targetPoints[0][1]))
-      console.log("Position threshold: " + POSITION_THRESHOLD)
-      if (Math.abs(lat - targetPoints[0][0]) < POSITION_THRESHOLD && Math.abs(lon - targetPoints[0][1]) < POSITION_THRESHOLD) {
+      console.log('Latitude distance: ' + Math.abs(lat - targetPoints[0][0]))
+      console.log('Longitude distance: ' + Math.abs(lon - targetPoints[0][1]))
+      console.log('Position threshold: ' + POSITION_THRESHOLD)
+      if (
+        Math.abs(lat - targetPoints[0][0]) < POSITION_THRESHOLD &&
+        Math.abs(lon - targetPoints[0][1]) < POSITION_THRESHOLD
+      ) {
         //targetPoints.shift()
       }
       console.log(targetPoints)
--- src/components/navigation/WaypointNav.jsx
+++ src/components/navigation/WaypointNav.jsx
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from 'react-redux'
 import React, {useState, useEffect} from 'react'
-import {requestWaypointNav} from '../../store/waypointNavSlice'
+import {requestWaypointNav, selectLongitude} from '../../store/waypointNavSlice'
 import {selectOpMode} from '../../store/opModeSlice'
 import './WaypointNav.css'
 
@@ -10,24 +10,75 @@ function WaypointNav() {
   const [points, setPoints] = useState([])
   const [lat, setLat] = useState(0)
   const [lon, setLon] = useState(0)
+  const [editingPoint, setEditingPoint] = useState(false)
+  var [editPointIndex, setEditPointIndex] = useState(0);
   var opMode = useSelector(selectOpMode)
 
+  //either adds point if in add point state or edits point if in editing state
   function addPoint() {
+    const newPoints = points.slice()
     const point = [lat, lon]
     //slicing because just copying the list leads to the html not re-rendering when lat & lon are 0
     //this is an edge case, but still
-    const newPoints = points.slice();
     newPoints.push(point)
     setPoints(newPoints)
     setLat(0)
     setLon(0)
   }
 
+  function editPoint() {
+    const newPoints = points.slice()
+    const point = [lat, lon]
+    console.log(point)
+    newPoints[editPointIndex] = point
+    console.log(newPoints[editPointIndex])
+    //newPoints[index][0] = point[0]
+    //newPoints[index][1] = point[1]
+    setPoints(newPoints)
+    setLat(0)
+    setLon(0)
+    setEditingPoint(false)
+  }
+
   function removePoint(index) {
     //slicing because just copying the list leads to the html not re-rendering
-    const newPoints = points.slice();
+    const newPoints = points.slice()
     newPoints.splice(index, 1)
     setPoints(newPoints)
+  }
+
+  //move point by swapping values of the point at index & point one above
+  function movePointUp(index) {
+    //slicing because just copying the list leads to the html not re-rendering
+    const newPoints = points.slice()
+    const indexLat = newPoints[index][0]
+    const indexLon = newPoints[index][1]
+    newPoints[index][0] = newPoints[index - 1][0]
+    newPoints[index][1] = newPoints[index - 1][1]
+    newPoints[index - 1][0] = indexLat
+    newPoints[index - 1][1] = indexLon
+    setPoints(newPoints)
+  }
+
+  //move point by swapping values of the point at index & point one below
+  function movePointDown(index) {
+    //slicing because just copying the list leads to the html not re-rendering
+    const newPoints = points.slice()
+    const indexLat = newPoints[index][0]
+    const indexLon = newPoints[index][1]
+    newPoints[index][0] = newPoints[index + 1][0]
+    newPoints[index][1] = newPoints[index + 1][1]
+    newPoints[index + 1][0] = indexLat
+    newPoints[index + 1][1] = indexLon
+    setPoints(newPoints)
+  }
+
+  //change the state to editing and change the values in the point fill to be the values in the point we're editing
+  function setEditPointState(index) {
+    setEditingPoint(true)
+    setEditPointIndex(index)
+    setLat(points[index][0])
+    setLon(points[index][1])
   }
 
   function handleSubmit(e) {
@@ -69,7 +120,9 @@ function WaypointNav() {
   }, [opMode])
 
   return (
-    <div className="waypoint-nav"> {/* only here to create a parent element for the form and the point list */}
+    <div className="waypoint-nav">
+      {' '}
+      {/* only here to create a parent element for the form and the point list */}
       <form method="post" onSubmit={handleSubmit} className="waypoint-select">
         <div className="waypoint-select__params">
           <label htmlFor="latitude">Latitude</label>
@@ -87,7 +140,10 @@ function WaypointNav() {
           {/* temp note: currently sends first value of points in latitude and longitude for testing, eventually want to switch to
           only using the points array. */}
           <input type="hidden" value={points.length > 0 ? points[0][0] : 0} name="latitude"></input>
-          <input type="hidden" value={points.length > 0 ? points[0][1] : 0} name="longitude"></input>
+          <input
+            type="hidden"
+            value={points.length > 0 ? points[0][1] : 0}
+            name="longitude"></input>
           <input type="hidden" value={JSON.stringify(points)} name="points"></input>
 
           {submitted ? (
@@ -97,9 +153,17 @@ function WaypointNav() {
               Copy from Clipboard
             </button>
           )}
-          <button type="button" onClick={addPoint}>
-            Add Point
-          </button>
+          {submitted ? (
+            <button disabled>Add Point</button>
+          ) : (editingPoint ? (
+            <button type="button" onClick={editPoint}>
+              Edit Point {editPointIndex + 1}
+            </button>
+          ) : (
+            <button type="button" onClick={addPoint}>
+              Add Point
+            </button>
+          ))}
         </div>
         <div className="waypoint-checkbox">
           <label>
@@ -126,21 +190,26 @@ function WaypointNav() {
         )}
       </form>
       <div className="waypoint-array">
-        <ol>{points.map((point, index) => 
-          <li key={index}>
-            <p>
-              lat: {point[0]} 
-              <br></br>
-              lon: {point[1]}
-            </p>
-            <button type="button" className="remove-points" onClick={() => removePoint(index)}>Remove</button>
-          </li>
-          )}
+        <ol>
+          {points.map((point, index) => (
+            <li key={index} style={{ color: (editingPoint && editPointIndex == index)? 'red' : 'white' }}>
+              <p>
+                lat: {point[0]}
+                <br></br>
+                lon: {point[1]}
+              </p>
+              <button type="button" className="remove-points" onClick={() => removePoint(index)}>
+                Remove
+              </button>
+              <button type="button" className="edit-points" onClick={() => setEditPointState(index)}>Edit</button>
+              <button hidden={index == 0} type="button" className="move-points" onClick={() => movePointUp(index)}>Move Up</button>
+              <button hidden={index == points.length - 1} type="button" className="move-points" onClick={() => movePointDown(index)}>Move Down</button>
+            </li>
+          ))}
         </ol>
       </div>
     </div>
   )
 }
-
 
 export default WaypointNav
