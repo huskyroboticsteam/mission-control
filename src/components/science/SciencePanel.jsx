76702562fc315644509797324fd68b29738f5525
elianaakim <140842030+elianaakim@users.noreply.github.com>
2025-05-26 12:29:35 -0700

    Drill on and off buttons (#91)
    * Drill on and off buttons
    
    * Functional buttons
    
    * edited power to integer
    
    * fixed drill on and off button
    
    * format fixed
    
    * changed the button text
    
    * enabled turning on and off the drill by clicking 'b' on keyboard
    
    * enabled keyboard 'b' to turn the drill on and off
    
    * moved 'switch to autonomous' button to the sidebar
    
    * moved autonomous button to sidebar
    
    * Revert "moved autonomous button to sidebar"
    
    This reverts commit 6df4ad54d19d73489f20a390589cf0e53c9d83a7.
    
    * Revert "moved 'switch to autonomous' button to the sidebar"
    
    This reverts commit 43a37afd3794aa85b4ff9a4a3a5740db1bca99fc.
    
    * created update drillMotor function in inputMiddleware to turn the drill on and off with 'b' button on keyboard
    
    * letting inputMiddleware update drill motor status
    
    * changed inputMiddleware to accept drillMotor instead of drillEnabled
    
    * edited to drillMotors from drillEnabled
    
    * changed button colors to red when disabled and drill is not on
    
    * changed the drill button colors
    
    * fixed drill on and off label
    
    * fixed drill on and off labels
    
    * added drillActuator
    
    * added drillActuator
    
    * drillActuator added
    
    * drillActuator
    
    * fixed unnecessary codes
    
    * fixed errors

--- src/components/science/SciencePanel.jsx
+++ src/components/science/SciencePanel.jsx
@@ -1,12 +1,27 @@
 import CameraStream from '../camera/CameraStream'
-
 import './SciencePanel.css'
+import {useSelector, useDispatch} from 'react-redux'
+import {selectDrillMotor, toggleDrillMotor} from '../../store/inputSlice'
 
 function ArmDexterityPanel() {
+  const dispatch = useDispatch()
+  const isDrillOn = useSelector(selectDrillMotor)
+
+  const handleClick = () => {
+    dispatch(toggleDrillMotor())
+  }
+
+  const className =
+    'enable-drill-button enable-drill-button--' + (isDrillOn ? 'enabled' : 'disabled')
+  const text = isDrillOn ? 'Turn off Drill' : 'Turn on Drill'
+
   return (
     <div className="science-panel">
       <CameraStream cameraName="pano" />
       <CameraStream cameraName="drill" />
+      <div className={className}>
+        <button onClick={handleClick}>{text}</button>
+      </div>
     </div>
   )
 }
