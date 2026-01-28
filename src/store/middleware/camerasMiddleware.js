a28cc8a81f89ea84b0c696521dc9222ba84b842d
Arjun Khamkar <66504973+ARRYTHEBEAST@users.noreply.github.com>
2026-01-28 13:22:36 +0530

    Telemetry metadata in camera stream images (#89)
    * import required libs
    
    * add telemetry metadata to downloaded images
    
    * prettify the code style
    
    * get a new position object from message
    
    * get lat and lon from message object
    
    * switch case fix
    
    * added accuracy for longtitude/lattitude
    
    * Added altitude and heading data
    
    * formatted pretty
    
    * Minor Changes; got rid of log statements and unused import stuff
    
    ---------
    
    Co-authored-by: Ceeli06 <cherylli621@gmail.com>

--- src/store/middleware/camerasMiddleware.js
+++ src/store/middleware/camerasMiddleware.js
@@ -11,6 +11,8 @@ import {
   roverDisconnected,
 } from '../roverSocketSlice'
 import camelCaseToTitle from '../../util/camelCaseToTitle'
+import {piexif} from 'piexifjs'
+import {Quaternion, Euler} from '@math.gl/core'
 
 /**
  * Middleware that handles requesting and receiving camera streams from the
@@ -102,8 +104,81 @@ const camerasMiddleware = (store) => (next) => (action) => {
           })
         )
       } else if (message.type === 'cameraFrameReport' && message.data !== '') {
+        let jpegData = `data:image/jpeg;base64,${message.data}`
+        let out = jpegData
+
+        // Fits the telemetry(position/gps) data into exif metadata
+        let gpsIfd = {}
+
+        // put altitude, max precision to prevent pack error @ tallest point in Earth
+        gpsIfd[piexif.GPSIFD.GPSAltitude] = [message.alt * 100000, 100000]
+
+        // converts & puts latitude data (decimal --> dms)
+        const lat = Math.abs(message.lat)
+        const latRef = message.lat >= 0 ? 'N' : 'S'
+        gpsIfd[piexif.GPSIFD.GPSLatitudeRef] = latRef
+
+        let degreesLat = Math.floor(lat) // takes integer value of lat
+        let minutesLat = Math.floor((lat - degreesLat) * 60) // takes decimal value of lat then * 60
+        let secondsLat = (((lat - degreesLat) * 60) % 1) * 60
+
+        gpsIfd[piexif.GPSIFD.GPSLatitude] = [
+          [degreesLat, 1],
+          [minutesLat, 1],
+          [secondsLat * 1000000, 1000000], // increases precision shown
+        ]
+
+        const lon = Math.abs(message.lon)
+        const lonRef = message.lon >= 0 ? 'E' : 'W'
+        gpsIfd[piexif.GPSIFD.GPSLongitudeRef] = lonRef
+
+        // converts & puts longitude data (decimal --> dms)
+        let degreesLon = Math.floor(lon) // takes integer value of lon
+        let minutesLon = Math.floor((lon - degreesLon) * 60) // takes decimal value of lon then * 60
+        let secondsLon = (((lon - degreesLon) * 60) % 1) * 60
+
+        gpsIfd[piexif.GPSIFD.GPSLongitude] = [
+          [degreesLon, 1],
+          [minutesLon, 1],
+          [secondsLon * 1000000, 1000000], // increases precision shown
+        ]
+
+        gpsIfd[piexif.GPSIFD.GPSDateStamp] = new Date()
+          .toISOString()
+          .slice(0, 10)
+          .replace(/-/g, ':')
+
+        gpsIfd[piexif.GPSIFD.GPSTimeStamp] = [
+          [new Date().getUTCHours(), 1],
+          [new Date().getUTCMinutes(), 1],
+          [new Date().getUTCSeconds(), 1],
+        ]
+
+        // Heading
+        const orientX = message.orientX
+        const orientY = message.orientY
+        const orientZ = message.orientZ
+        const orientW = message.orientW
+        let quat = new Quaternion(orientX, orientY, orientZ, orientW)
+        let rpy = new Euler().fromQuaternion(quat, Euler.ZYX)
+        let yaw = Math.round((rpy.yaw * 180) / Math.PI)
+        let heading = yaw != null ? -yaw : undefined
+
+        // heading is from -180 to 180 but to format it into metadata, has
+        // to be 0 --> 360 so we remap the negative values of heading
+        if (heading < 0) {
+          heading += 360
+        }
+
+        gpsIfd[piexif.GPSIFD.GPSImgDirection] = [heading, 1]
+        gpsIfd[piexif.GPSIFD.GPSImgDirectionRef] = 'M' //magnetic north
+
+        const exifObj = {GPS: gpsIfd}
+        const exifBytes = piexif.dump(exifObj)
+        out = piexif.insert(exifBytes, jpegData)
+
         let link = document.createElement('a')
-        link.href = `data:image/jpg;base64,${message.data}`
+        link.href = out
         let time = new Date()
         let timezoneOffset = time.getTimezoneOffset() * 60000
         let timeString = new Date(time - timezoneOffset)
