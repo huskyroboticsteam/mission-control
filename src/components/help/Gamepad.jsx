7fb78f47e9779d8822aeff6e22c1ebe3c8ef44a5
Ceeli06 <cherylli621@gmail.com>
2026-01-31 14:02:08 -0800

    Need help on Dynamic Keyboard Controls

---
+++ src/components/help/Gamepad.jsx
@@ -0,0 +1,81 @@
+import { setupPresetInteractiveGamepad } from "virtual-gamepad-lib/helpers";
+import { GamepadEmulator } from "virtual-gamepad-lib/GamepadEmulator";
+// import the svg source code for the left and right onscreen gamepad SVGs
+//  - this could be done like with a build tool like webpack or vite (shown here) or at runtime with a fetch request.
+import LEFT_GPAD_SVG_SOURCE_CODE from "virtual-gamepad-lib/gamepad_assets/rounded/display-gamepad-left.svg?raw";
+import FULL_GPAD_SVG_SOURCE_CODE from "virtual-gamepad-lib/gamepad_assets/rounded/display-gamepad-full.svg?raw";
+// A single GamepadEmulator MUST be created BEFORE initilizing a game engine, a GamepadApiWrapper, or any other library that listens for gamepad events (uses navigator.getGamepads())
+// This event is called when the page is fully loaded.
+
+function Gamepad() {
+    const gpadEmulator = new GamepadEmulator(0.1);
+
+    // Get the HTML elements we will be using to display the gamepad state
+    const GPAD_DISPLAY_CONTAINER = container;
+    //const AXIS_TABLE_ELEM = document.getElementById('axis-table');
+    //const BUTTON_TABLE_ELEM = document.getElementById('button-table');
+    // REQUIRED: Insert the SVG contents for the left gamepad into the DOM
+     // REQUIRED: Insert the SVG contents for the right gamepad into the DOM
+    // The index of the emulated gamepad our onscreen gamepad will show up as in the navigator.getGamepads() array.
+    const EMULATED_GPAD_INDEX = 0;
+    // setup the onscreen gamepad to react to the state of the emulated gamepad.
+    const { gpadApiWrapper } = setupPresetInteractiveGamepad(GPAD_DISPLAY_CONTAINER, {
+        AllowDpadDiagonals: true,
+        GpadEmulator: gpadEmulator,
+        EmulatedGamepadIndex: EMULATED_GPAD_INDEX,
+        EmulatedGamepadOverlayMode: true,
+        /* for more option, see interactiveGamepadPresetConfig type in helpers.ts */
+    });
+    // listen for gamepad button change events and display them (you could also check for changes yourself by calling the navigator.getGamepads() function)
+    gpadApiWrapper.onGamepadButtonChange((gpadIndex, gpad, buttonChanges) => {
+        console.log(`Gamepad ${gpadIndex} button change: `, buttonChanges);
+        // only update the table for the emulated gamepad (not other connected gamepads)
+        if (gpadIndex !== EMULATED_GPAD_INDEX)
+            return;
+        // loop through each button and update the table for the buttons that changed
+        for (let i = 0; i < gpad.buttons.length; i++) {
+            if (!buttonChanges[i])
+                continue; // this button did not change, so skip it.
+            // get the numerical value of the button, 0 for not pressed, 1 for fully pressed, and a value in between for partially pressed
+           // const btnValue = gpad.buttons[i].value;
+            // get the table row that displays the button values
+            //const btnTableRow = BUTTON_TABLE_ELEM.children[i];
+            //if (!btnTableRow)
+              //  continue;
+            // get the table cell that displays the button value
+            //const btnValueCell = btnTableRow.children[2];
+            // set the button cell background color based on the button state, blueviolet for pressed, greenyellow for touched, and default color for not pressed or touched
+            //btnTableRow.style.backgroundColor = gpad.buttons[i].pressed ? "blueviolet" : (gpad.buttons[i].touched ? "greenyellow" : "");
+            // set the button value table cell background color based on the numerical button value, blue for fully pressed, and default color for not pressed
+            //btnValueCell.style.backgroundColor = btnValue == 0 ? "" : "#00FF" + Math.round(btnValue * 255).toString(16).padStart(2, "0");
+            // display the numerical button value in the table
+            //btnValueCell.innerText = btnValue.toFixed(2);
+        }
+    });
+    // listen for gamepad axis change events and log them (you could also check for changes yourself by calling the navigator.getGamepads() function)
+    gpadApiWrapper.onGamepadAxisChange((gpadIndex, gpad, axisChangesMask) => {
+        console.log(`Gamepad ${gpadIndex} axis change: `, gpad.axes, axisChangesMask);
+        // only update the table for the emulated gamepad (not other connected gamepads)
+        if (gpadIndex !== EMULATED_GPAD_INDEX)
+            return;
+        // loop through each axis and update the table for the axes that changed
+        for (let i = 0; i < axisChangesMask.length; i++) {
+            if (!axisChangesMask[i])
+                continue; // this axis did not change, so skip it
+            // otherwise display the axis change in the table:
+            //const axisValuesTableRow = AXIS_TABLE_ELEM.children[1];
+            //const axisValueCell = axisValuesTableRow.children[i + 1];
+            if (!axisValueCell)
+                continue;
+            // get the numerical value of the axis, a value between -1 and 1
+            const axisValue = gpad.axes[i];
+            console.log(axisValue);
+            // set the axis value table cell background color based on the numerical axis value, more red for positive, more blue for negative, and neutral for 0
+            //axisValueCell.style.backgroundColor = "#" + Math.round(255 - (Math.max(-axisValue, 0) * 255)).toString(16).padStart(2, "0") + "FF" + Math.round(255 - (Math.max(axisValue, 0) * 255)).toString(16).padStart(2, "0");
+            // display the numerical axis value in the table
+            //axisValueCell.innerText = axisValue.toFixed(2);
+        }
+    });
+}
+
+export default Gamepad;
--- src/components/help/HelpPanel.jsx
+++ src/components/help/HelpPanel.jsx
@@ -1,18 +1,100 @@
 import './HelpPanel.css'
-import keyboardControls from './keyboardControls.png'
-import standardDriveControls from './standardDriveControls.png'
-import tankDriveControls from './tankDriveControls.png'
-import armControls from './armControls.png'
+import { GamepadApiWrapper } from "virtual-gamepad-lib/GamepadApiWrapper";
+import { GamepadEmulator, DEFAULT_GPAD_BUTTON_COUNT, DEFAULT_GPAD_AXIS_COUNT } from "virtual-gamepad-lib/GamepadEmulator";
+import { GamepadDisplay } from "virtual-gamepad-lib/GamepadDisplay";
+import { gamepadButtonType, gamepadDirection, gamepadEmulationState, PRESET_SVG_GPAD_BTN_IDS, PRESET_SVG_GPAD_BTN_TAP_TARGET_IDS, PRESET_SVG_GPAD_CLASS, standardGpadButtonMap } from "virtual-gamepad-lib/enums";
+import { CenterTransformOrigin } from "virtual-gamepad-lib/utilities";
+import React, { useState, useRef, useEffect } from "react";
+import Gamepad from './Gamepad.jsx';
+import FULL_GPAD_SVG_SOURCE_CODE from "virtual-gamepad-lib/gamepad_assets/rounded/display-gamepad-full.svg?raw";
+
+function FullGpad() {
+  return (
+    <div>
+      <Gamepad style={{ width: 200, height: 200}}
+      dangerouslySetInnerHTML={{ __html: FULL_GPAD_SVG_SOURCE_CODE }}/>
+    </div>
+  );
+}
+
+function addEmulatedGamepad(overlay, index, gpadEmulator) {
+        if (overlay) {
+            // add an emulated gamepad at the provided index with overlayMode on.
+            gpadEmulator.AddEmulatedGamepad(index, true, DEFAULT_GPAD_BUTTON_COUNT, DEFAULT_GPAD_AXIS_COUNT); // returns the new (emulated) gamepad or false if some error happened.
+        }
+        else {
+            // add an emulated Gamepad at the next available index (indicated by -1) with overlayMode off.
+            gpadEmulator.AddEmulatedGamepad(-1, false, DEFAULT_GPAD_BUTTON_COUNT, DEFAULT_GPAD_AXIS_COUNT); // returns the new (emulated) gamepad or false if some error happened.
+        }
+    }
+
+function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
+        /* ----- SETUP BUTTON TOUCH INPUTS ----- */
+        gpadEmulator.AddButtonTouchEventListeners(gpadIndex, PRESET_SVG_GPAD_BTN_TAP_TARGET_IDS.map((tapTargetId, i) => {
+            const isTrigger = tapTargetId.includes("trigger");
+            const isStick = tapTargetId.includes("stick");
+            if (isTrigger) {
+                // trigger buttons usually take variable pressure so can be represented by a variable button that is dragged down.
+                return {
+                    buttonIndex: i,
+                    type: gamepadButtonType.variable,
+                    dragDistance: 50, // pixels that the user must drag the button down to fully press it.
+                    lockTargetWhilePressed: true,
+                    directions: {
+                        [gamepadDirection.up]: false,
+                        [gamepadDirection.down]: true,
+                        [gamepadDirection.left]: false,
+                        [gamepadDirection.right]: false,
+                    }
+                };
+            }
+            else {
+                return {
+                    buttonIndex: i,
+                    type: gamepadButtonType.onOff,
+                    lockTargetWhilePressed: (isStick === true),
+                };
+            }
+        }));
+        /* ----- SETUP JOYSTICK TOUCH INPUTS ----- */
+        gpadEmulator.AddJoystickTouchEventListeners(gpadIndex, [
+            {
+                // left joystick (we re-use the same tap target id as the button for clicking the left stick)
+                dragDistance: 30, // pixels that the user must drag the joystic to represent + 1 (or in reverse to represent minus one).
+                xAxisIndex: 0,
+                yAxisIndex: 1,
+                lockTargetWhilePressed: true,
+                directions: {
+                    [gamepadDirection.up]: true,
+                    [gamepadDirection.down]: true,
+                    [gamepadDirection.left]: true,
+                    [gamepadDirection.right]: true,
+                },
+            },
+            {
+                // right joystick (we re-use the same tap target id as the button for clicking the right stick)
+                dragDistance: 30, // pixels that the user must drag the joystic to represent + 1 (or in reverse to represent minus one).
+                xAxisIndex: 2,
+                yAxisIndex: 3,
+                lockTargetWhilePressed: true,
+                directions: {
+                    [gamepadDirection.up]: true,
+                    [gamepadDirection.down]: true,
+                    [gamepadDirection.left]: true,
+                    [gamepadDirection.right]: true,
+                },
+            }
+        ]);
+    }
+
 
 function HelpPanel() {
   return (
     <div className="help-panel">
-      <img src={standardDriveControls} alt="standard drive controls" />
-      <img src={tankDriveControls} alt="tank drive controls" />
-      <img src={armControls} alt="arm controls" />
-      <img src={keyboardControls} alt="keyboard controls" />
+      <FullGpad />
     </div>
   )
 }
 
-export default HelpPanel
+export default HelpPanel;
+
