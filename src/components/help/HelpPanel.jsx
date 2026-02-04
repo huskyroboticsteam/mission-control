4dec93f625f45d0d0199585e41eda009c6f52692
Ceeli06 <cherylli621@gmail.com>
2026-02-03 19:59:37 -0800

    Figured out how to get SVG to show, fixed error where Gamepad obj was created more than once, trying to figure out how to map input but mapping is undefined so all buttons are currently undefined...

--- src/components/help/HelpPanel.jsx
+++ src/components/help/HelpPanel.jsx
@@ -5,28 +5,9 @@ import { GamepadDisplay } from "virtual-gamepad-lib/GamepadDisplay";
 import { gamepadButtonType, gamepadDirection, gamepadEmulationState, PRESET_SVG_GPAD_BTN_IDS, PRESET_SVG_GPAD_BTN_TAP_TARGET_IDS, PRESET_SVG_GPAD_CLASS, standardGpadButtonMap } from "virtual-gamepad-lib/enums";
 import { CenterTransformOrigin } from "virtual-gamepad-lib/utilities";
 import React, { useState, useRef, useEffect } from "react";
-import Gamepad from './Gamepad.jsx';
+import GamepadSvg from './GamepadSvg.jsx';
 import FULL_GPAD_SVG_SOURCE_CODE from "virtual-gamepad-lib/gamepad_assets/rounded/display-gamepad-full.svg?raw";
-
-function FullGpad() {
-  return (
-    <div>
-      <Gamepad style={{ width: 200, height: 200}}
-      dangerouslySetInnerHTML={{ __html: FULL_GPAD_SVG_SOURCE_CODE }}/>
-    </div>
-  );
-}
-
-function addEmulatedGamepad(overlay, index, gpadEmulator) {
-        if (overlay) {
-            // add an emulated gamepad at the provided index with overlayMode on.
-            gpadEmulator.AddEmulatedGamepad(index, true, DEFAULT_GPAD_BUTTON_COUNT, DEFAULT_GPAD_AXIS_COUNT); // returns the new (emulated) gamepad or false if some error happened.
-        }
-        else {
-            // add an emulated Gamepad at the next available index (indicated by -1) with overlayMode off.
-            gpadEmulator.AddEmulatedGamepad(-1, false, DEFAULT_GPAD_BUTTON_COUNT, DEFAULT_GPAD_AXIS_COUNT); // returns the new (emulated) gamepad or false if some error happened.
-        }
-    }
+import { setupPresetInteractiveGamepad } from 'virtual-gamepad-lib/helpers';
 
 function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
         /* ----- SETUP BUTTON TOUCH INPUTS ----- */
@@ -38,6 +19,7 @@ function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
                 return {
                     buttonIndex: i,
                     type: gamepadButtonType.variable,
+                    tapTarget: display_gpad.querySelector("#" + tapTargetId),
                     dragDistance: 50, // pixels that the user must drag the button down to fully press it.
                     lockTargetWhilePressed: true,
                     directions: {
@@ -53,6 +35,7 @@ function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
                     buttonIndex: i,
                     type: gamepadButtonType.onOff,
                     lockTargetWhilePressed: (isStick === true),
+                    tapTarget: display_gpad.querySelector("#" + tapTargetId)
                 };
             }
         }));
@@ -60,6 +43,7 @@ function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
         gpadEmulator.AddJoystickTouchEventListeners(gpadIndex, [
             {
                 // left joystick (we re-use the same tap target id as the button for clicking the left stick)
+                tapTarget: display_gpad.querySelector("#" + PRESET_SVG_GPAD_BTN_TAP_TARGET_IDS[standardGpadButtonMap.LStick]),
                 dragDistance: 30, // pixels that the user must drag the joystic to represent + 1 (or in reverse to represent minus one).
                 xAxisIndex: 0,
                 yAxisIndex: 1,
@@ -73,6 +57,7 @@ function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
             },
             {
                 // right joystick (we re-use the same tap target id as the button for clicking the right stick)
+                tapTarget: display_gpad.querySelector("#" + PRESET_SVG_GPAD_BTN_TAP_TARGET_IDS[standardGpadButtonMap.RStick]),
                 dragDistance: 30, // pixels that the user must drag the joystic to represent + 1 (or in reverse to represent minus one).
                 xAxisIndex: 2,
                 yAxisIndex: 3,
@@ -87,14 +72,34 @@ function setupEmulatedGamepadInput(gpadIndex, display_gpad, gpadEmulator) {
         ]);
     }
 
-
 function HelpPanel() {
+  const gamepadEmulator = useRef(null);
+  const gamepadApiWrapper = useRef(null);
+  const displayGpad = useRef(null);
+  
+  useEffect(() => {
+    if (gamepadEmulator.current || gamepadApiWrapper.current) return;
+
+    gamepadEmulator.current = new GamepadEmulator(0.1);
+    gamepadApiWrapper.current = setupPresetInteractiveGamepad(displayGpad.current, {
+      GpadEmulator: gamepadEmulator.current,
+      AllowDpadDiagonals: true,
+      EmulatedGamepadOverlayMode: true,
+      gpadIndex: 0,
+    });
+    
+    //new GamepadApiWrapper({ buttonConfigs: [], updateDelay: 0, axisDeadZone: 0.05 });
+    gamepadEmulator.current.AddEmulatedGamepad(0, true, 18, 4);
+    setupEmulatedGamepadInput(0, displayGpad.current, gamepadEmulator.current);
+  }, []);
+
+
   return (
     <div className="help-panel">
-      <FullGpad />
+      <div className="gamepad-1" ref ={displayGpad} dangerouslySetInnerHTML={{__html: FULL_GPAD_SVG_SOURCE_CODE}}>
+      </div>
     </div>
   )
 }
 
 export default HelpPanel;
-
