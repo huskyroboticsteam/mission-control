a9de9d67b9a683207c26cca7453bbcefd09f4de0
Husky Robotics <huskyrobotics@system76-pc.localdomain>
2026-02-17 19:36:35 -0800

    Fix backend python script

--- src/util/network_backend.py
+++ src/util/network_backend.py
@@ -2,21 +2,43 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import requests
+import urllib3
+
+# Custom Adapter to disable security because Rocket firmware is so old
+class CustomHTTPAdapter(requests.adapters.HTTPAdapter):
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+    
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
 
 app = FastAPI()
 s = requests.Session()
-BASE = "http://10.42.0.3/"
+
+# Downgrading security
+ctx = urllib3.util.create_urllib3_context()
+ctx.set_ciphers("DEFAULT@SECLEVEL=0")
+ctx.check_hostname=False
+
+# Adding custom adapter to https:// route
+s.adapters.pop("https://", None)
+s.mount("https://", CustomHTTPAdapter(ctx))
+
+BASE = "https://10.42.0.4/"
 
 @app.get("/api/ubnt/sta")
 def sta():
-    s.get(BASE + "login.cgi")
-    s.post(BASE + "login.cgi", files={
+    s.get(BASE + "login.cgi", verify=False)
+    s.post(BASE + "login.cgi", verify=False, files={
         "username": (None, "ubnt"),
         "password": (None, "huskerRubbot3"),
         "uri": (None, "/"),
     })
-    r = s.get(BASE + "sta.cgi")
-    return r
+    r = s.get(BASE + "sta.cgi", verify=False)
+    return r.content
 
 app.add_middleware(
     CORSMiddleware,
