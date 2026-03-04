d3da30efffc9731679ed3a6389f738e45ba9e929
Ceeli06 <cherylli621@gmail.com>
2026-03-04 14:55:15 -0800

    task started

--- src/components/navigation/NavigationPanel.jsx
+++ src/components/navigation/NavigationPanel.jsx
@@ -4,16 +4,45 @@ import './NavigationPanel.css'
 import OpModeSelect from './OpModeSelect'
 import WaypointList from './WaypointList'
 import WaypointNav from './WaypointNav'
+import {useState} from 'react'
 
 function NavigationPanel() {
+
+  const [camMode, setCamMode] = useState("all");
+  console.log(camMode);
+
+  const handleChange = (event) => {
+    setCamMode(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div className="navigation-panel">
-      <CameraStream camera="mast" />
-      <CameraStream camera="hand" />
-      <Compass />
-      <CameraStream camera="wrist" />
-      <WaypointList />
-      <WaypointNav />
+      <select className = "dropdown" onClick = {handleChange}>
+        <option value= "all">All Cameras</option>
+        <option value="mast">Mast Camera</option>
+        <option value="hand">Hand Camera</option>
+        <option value="wrist">Wrist Camera</option>
+      </select>
+      <div className = "cameras">
+        {camMode === "mast" && <CameraStream camera="mast" />}
+        {camMode === "hand" && <CameraStream camera="hand" />}
+        {camMode === "wrist" && <CameraStream camera="wrist" />}
+        {camMode === "all" && (
+          <>
+            <CameraStream camera="mast" />
+            <CameraStream camera="hand" />
+            <CameraStream camera="wrist" />
+          </>
+        )}
+      </div>
+      
+
+      <div className = "compassAndWaypoint">
+        <Compass />
+        <WaypointList />
+        <WaypointNav />
+      </div>
     </div>
   )
 }
