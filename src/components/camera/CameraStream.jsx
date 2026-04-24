19fd30b59d4924adc5cc03151db4551b8ccca0b5
vabhi <vabhi2007@gmail.com>
2026-04-23 18:00:48 -0700

    Update CameraStream.jsx

--- src/components/camera/CameraStream.jsx
+++ src/components/camera/CameraStream.jsx
@@ -172,7 +172,7 @@ function CameraStream({camera}) {
         node: `${camera}-player`,
         mode: 'video',
         flushingTime: 0,
-        videoCodec: 'h264',
+        videoCodec: 'h265',
         maxDelay: 50,
         clearBuffer: true,
         onError: function (data) {
