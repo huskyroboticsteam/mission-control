a7cc267148ceb1b2de18c22cb0c9a2df2a787339
Eliana <yunmin626@gmail.com>
2026-05-07 19:18:38 -0700

    lat and lon as pure ints

--- src/store/middleware/waypointNavMiddleware.js
+++ src/store/middleware/waypointNavMiddleware.js
@@ -16,6 +16,8 @@ const waypointNavMiddleware = (store) => (next) => (action) => {
               tag: first.tag ?? '',
               circleMode: first.circleMode ?? false,
               radius: first.radius ?? 0,
+              lat,
+              lon,
               //points: pts.map(({lat, lon}) => ({latitude: lat, longitude: lon})),
             }
           })(),
