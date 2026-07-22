504c150b9fc9a5090a2eca7f83ed8a7e97388308
Isaac Wu <isaacwu@uw.edu>
2026-07-21 21:21:46 -0700

    add host, disable motors on disconnect

--- src/constants/networkConstants.ts
+++ src/constants/networkConstants.ts
@@ -1,4 +1,4 @@
 export const IS_DEV = process.env.NODE_ENV === 'development'
-export const ROVER_HOSTS = ['jetson-1.local', 'jetson-2.local']
+export const ROVER_HOSTS = ['jetson-1.local', 'jetson-2.local', 'isaac-jetson.local']
 
 export const CONNECTION_ATTEMPT_DELAY = 5000
